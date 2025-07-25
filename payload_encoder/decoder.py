e089960ed3f168cd24760d4162d490fa20232b37
Vyzer <samzoovsk19@gmail.com>
2025-07-25 02:52:17 -0300

    Atualizar o decoder.py

--- payload_encoder/decoder.py
+++ payload_encoder/decoder.py
@@ -4,4 +4,4 @@ from stegano.lsb import decode_lsb
 def extract_payload_image(imagem:str,key:bytes) -> dict:
 data = decode_lsb(image)
 message = secure_decode(data,key)
-rerurn message
+return message
