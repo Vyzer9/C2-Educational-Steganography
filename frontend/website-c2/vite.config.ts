e154c20c879f7cc0e912f6c986f813b3221447c2
Vyzer9 <Samzoovsk19@gmail.com>
2025-09-07 23:27:57 -0300

    up

--- frontend/website-c2/vite.config.ts
+++ frontend/website-c2/vite.config.ts
@@ -13,7 +13,6 @@ export default defineConfig(({ mode }) => ({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "/": path.resolve(__dirname, "./src"),
     },
   },
 }));
