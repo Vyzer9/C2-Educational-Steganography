abcf77a3637b75ca20f68761f9c63ad99d4a0290
Vyzer9 <Samzoovsk19@gmail.com>
2025-09-08 15:12:43 -0300

    up

--- api/main.py
+++ api/main.py
@@ -1,4 +1,3 @@
-# main.py
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from api.routes import embed, extract
@@ -8,11 +7,17 @@ app = FastAPI()
 # Configuração do CORS - permitir o frontend que roda em localhost:8080 acessar a API
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://c2-steganography-phi.vercel.app/"],  # coloque aqui a URL do seu frontend
+    allow_origins=["http://localhost:8080"],  # coloque aqui a URL do seu frontend
     allow_credentials=True,
     allow_methods=["*"],  # permite todos os métodos (GET, POST, etc)
     allow_headers=["*"],  # permite todos os headers
 )
 
+# Inclusão das rotas da API
 app.include_router(embed.router, prefix="/embed", tags=["embed"])
 app.include_router(extract.router, prefix="/extract", tags=["extract"])
+
+# ✅ Rota raiz para o Render fazer o health check
+@app.get("/")
+def read_root():
+    return {"status": "API online"}
